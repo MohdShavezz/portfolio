b29183687bb1759ad2cb829a1f3c0c0b70bbf19c
Mohd Shavez <88038219+MohdShavezz@users.noreply.github.com>
2024-09-29 18:46:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,49 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     // On Click for Mini Projects and Certificates
-$('#mini-projs-title').click(function (e) {
-    e.stopPropagation(); // Prevents click event from bubbling up to document
-    $('#mini-projs').toggleClass('mini-projs-diplay-toggle');
-});
+    $('#mini-projs-title').click(function (e) {
+        e.stopPropagation(); // Prevents click event from bubbling up to document
+        $('#mini-projs').toggleClass('mini-projs-diplay-toggle');
+    });
 
-$('#certificates').click(function (e) {
-    e.stopPropagation(); // Prevents click event from bubbling up to document
-    $('#certi').toggleClass('mini-projs-diplay-toggle');
-});
+    $('#certificates').click(function (e) {
+        e.stopPropagation(); // Prevents click event from bubbling up to document
+        $('#certi').toggleClass('mini-projs-diplay-toggle');
+    });
 
-// Hide the element when clicking outside
-$(document).click(function (e) {
-    // Check if the click is outside of the mini-projs and certi containers
-    if (!$(e.target).closest('#mini-projs, #mini-projs-title').length) {
-        $('#mini-projs').removeClass('mini-projs-diplay-toggle');
-    }
-    if (!$(e.target).closest('#certi, #certificates').length) {
-        $('#certi').removeClass('mini-projs-diplay-toggle');
-    }
-});
-    
-    $(window).scroll(function(){
-        if(this.scrollY>20){
+    // Hide the element when clicking outside
+    $(document).click(function (e) {
+        // Check if the click is outside of the mini-projs and certi containers
+        if (!$(e.target).closest('#mini-projs, #mini-projs-title').length) {
+            $('#mini-projs').removeClass('mini-projs-diplay-toggle');
+        }
+        if (!$(e.target).closest('#certi, #certificates').length) {
+            $('#certi').removeClass('mini-projs-diplay-toggle');
+        }
+    });
+
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
             $('.navbar').addClass('sticky')
-        }else{
+        } else {
             $('.navbar').removeClass('sticky')
         }
     });
 
-    $('.menu-btn').click(function(){
+    $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass('act');
         $('.menu-btn i').toggleClass('act');
     });
     // owl carousel script 
     $('.carousel').owlCarousel({
-        margin:40,
-        loop:true,
-        autoplayTimeout:2000,
-        autoplayHoverpause:true,
-        responsive:{
-            0:{items:1,nav:false},
-            600:{items:2,nav:false},
-            1000:{items:3,nav:false}  
+        margin: 40,
+        loop: true,
+        autoplayTimeout: 2000,
+        autoplayHoverpause: true,
+        responsive: {
+            0: { items: 1, nav: false },
+            600: { items: 2, nav: false },
+            1000: { items: 3, nav: false }
         }
     });
 
@@ -51,15 +51,11 @@ $(document).click(function (e) {
     // $('.scrollupbtn').click(function(){
     //     $('html').animate({scrollTop:0});
     // });
-    $(".menu li a").click(function() {
+    $(".menu li a").click(function () {
         // Check if the parent <li> has neither the ID "mini-projs-title" nor "certificates"
         if ($(this).parent().attr('id') !== 'mini-projs-title' && $(this).parent().attr('id') !== 'certificates') {
             $('.navbar .menu').removeClass('act');
             $('.menu-btn i').removeClass('act');
         }
     });
-    
-    
-
-
 });
